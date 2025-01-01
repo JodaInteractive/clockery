7749159f9f6a87c89977d1f358d4564e6867507e
Joda <JodaInteractive@protonmail.com>
2025-01-01 15:30:16 -0600

    wip

--- aws/aws.go
+++ aws/aws.go
@@ -1,73 +1,73 @@
 package main
 
 import (
-	"github.com/aws/aws-cdk-go/awscdk/v2"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
-	"github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
-	"github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
+	cdk "github.com/aws/aws-cdk-go/awscdk/v2"
+	apigateway "github.com/aws/aws-cdk-go/awscdk/v2/awsapigateway"
+	dynamo "github.com/aws/aws-cdk-go/awscdk/v2/awsdynamodb"
+	lambda "github.com/aws/aws-cdk-go/awscdklambdagoalpha/v2"
 	"github.com/aws/constructs-go/constructs/v10"
 	"github.com/aws/jsii-runtime-go"
 )
 
 type ClockeryStackProps struct {
-	awscdk.StackProps
+	cdk.StackProps
 }
 
-func NewAwsStack(scope constructs.Construct, id string, props *ClockeryStackProps) awscdk.Stack {
-	var sprops awscdk.StackProps
+func NewAwsStack(scope constructs.Construct, id string, props *ClockeryStackProps) cdk.Stack {
+	var sprops cdk.StackProps
 	if props != nil {
 		sprops = props.StackProps
 	}
-	stack := awscdk.NewStack(scope, &id, &sprops)
+	stack := cdk.NewStack(scope, &id, &sprops)
 
-	postLambda := awscdklambdagoalpha.NewGoFunction(stack, jsii.String("ClockeryLeaderboardPostFunction"), &awscdklambdagoalpha.GoFunctionProps{
+	postLambda := lambda.NewGoFunction(stack, jsii.String("ClockeryLeaderboardPostFunction"), &lambda.GoFunctionProps{
 		Entry: jsii.String("lambda/leaderboard/post/post.go"),
 		Environment: &map[string]*string{
 			"TABLE_NAME": jsii.String("ClockeryLeaderboard"),
 		},
 	})
 
-	getLambda := awscdklambdagoalpha.NewGoFunction(stack, jsii.String("ClockeryLeaderboardGetFunction"), &awscdklambdagoalpha.GoFunctionProps{
+	getLambda := lambda.NewGoFunction(stack, jsii.String("ClockeryLeaderboardGetFunction"), &lambda.GoFunctionProps{
 		Entry: jsii.String("lambda/leaderboard/get-all/get.go"),
 		Environment: &map[string]*string{
 			"TABLE_NAME": jsii.String("ClockeryLeaderboard"),
 		},
 	})
 
-	dynamo := awsdynamodb.NewTable(stack, jsii.String("ClockeryLeaderboard"), &awsdynamodb.TableProps {
-		PartitionKey: &awsdynamodb.Attribute{ Name: jsii.String("id"), Type: awsdynamodb.AttributeType_STRING },
-		TableName: jsii.String("ClockeryLeaderboard"),
+	dynamo := dynamo.NewTable(stack, jsii.String("ClockeryLeaderboard"), &dynamo.TableProps{
+		PartitionKey: &dynamo.Attribute{Name: jsii.String("id"), Type: dynamo.AttributeType_STRING},
+		TableName:    jsii.String("ClockeryLeaderboard"),
 	})
 	dynamo.GrantFullAccess(postLambda)
 	dynamo.GrantFullAccess(getLambda)
 
-	api := awsapigateway.NewRestApi(stack, jsii.String("ClockeryLeaderboardApi"), &awsapigateway.RestApiProps{
+	api := apigateway.NewRestApi(stack, jsii.String("ClockeryLeaderboardApi"), &apigateway.RestApiProps{
 		RestApiName: jsii.String("ClockeryLeaderboardApi"),
-		DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
-			AllowOrigins: awsapigateway.Cors_ALL_ORIGINS(),
-			AllowMethods: awsapigateway.Cors_ALL_METHODS(),
-			},
+		DefaultCorsPreflightOptions: &apigateway.CorsOptions{
+			AllowOrigins: apigateway.Cors_ALL_ORIGINS(),
+			AllowMethods: apigateway.Cors_ALL_METHODS(),
+		},
 	})
 
 	resource := api.Root().AddResource(
-		jsii.String("leaderboard"), 
-		&awsapigateway.ResourceOptions{
-			DefaultCorsPreflightOptions: &awsapigateway.CorsOptions{
-				AllowOrigins: awsapigateway.Cors_ALL_ORIGINS(),
-				AllowMethods: awsapigateway.Cors_ALL_METHODS(),
+		jsii.String("leaderboard"),
+		&apigateway.ResourceOptions{
+			DefaultCorsPreflightOptions: &apigateway.CorsOptions{
+				AllowOrigins: apigateway.Cors_ALL_ORIGINS(),
+				AllowMethods: apigateway.Cors_ALL_METHODS(),
 			},
 		},
 	)
-	
+
 	resource.AddMethod(
 		jsii.String("GET"),
-		awsapigateway.NewLambdaIntegration(getLambda, &awsapigateway.LambdaIntegrationOptions{}),
+		apigateway.NewLambdaIntegration(getLambda, &apigateway.LambdaIntegrationOptions{}),
 		nil,
 	)
 
 	resource.AddMethod(
 		jsii.String("POST"),
-		awsapigateway.NewLambdaIntegration(postLambda, &awsapigateway.LambdaIntegrationOptions{}),
+		apigateway.NewLambdaIntegration(postLambda, &apigateway.LambdaIntegrationOptions{}),
 		nil,
 	)
 
@@ -77,10 +77,10 @@ func NewAwsStack(scope constructs.Construct, id string, props *ClockeryStackProp
 func main() {
 	defer jsii.Close()
 
-	app := awscdk.NewApp(nil)
+	app := cdk.NewApp(nil)
 
 	NewAwsStack(app, "ClockeryStack", &ClockeryStackProps{
-		awscdk.StackProps{
+		cdk.StackProps{
 			Env: env(),
 		},
 	})
@@ -88,6 +88,6 @@ func main() {
 	app.Synth(nil)
 }
 
-func env() *awscdk.Environment {
+func env() *cdk.Environment {
 	return nil
 }
